b15edafdb1ec3eac4c67cbdd1730aa0459a4c37a
chiqvu1 <chiquynhvu268@gmail.com>
2019-10-15 17:02:24 -0700

    Created new resource file type menu. Informed system that title fragment has a menu. setHasOptionMenu to true in titleFragment.kt override onCreateOptionMenu override onOptionItemSelected

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -2,13 +2,13 @@ package com.example.android.navigation
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 import com.google.android.material.internal.NavigationMenuItemView
 
@@ -23,8 +23,19 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener (
                 Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
+        setHasOptionsMenu(true)
         return binding.root
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //if NavigationUI can not handle the menu, then super.onOption can handle the menu without navigating
+        //this to prevent crash
+        return NavigationUI.onNavDestinationSelected(item!!, view!!.findNavController()) || super.onOptionsItemSelected(item)
     }
 
 
